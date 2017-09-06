ae0fbab37ab4d9272ba359e7a6686e059d5923a7
Justin Frawley <freegamespeech@gmail.com>
2017-09-06 20:34:30 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(words) # put argument(s) here
+  x = 0
+  while x < words.length
+    yield (words[x])
+    x += 1
+  end
   # code here
+  words
 end
